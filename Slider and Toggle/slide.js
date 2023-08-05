b032a7e4521d20b4542cc1af23a14abd6a0acb23
fidanbh <113761182+fidanbh@users.noreply.github.com>
2023-08-05 22:02:51 +0400

    Update slide.js

--- Slider and Toggle/slide.js
+++ Slider and Toggle/slide.js
@@ -1,23 +1,32 @@
 let left = document.getElementById("leftIcon");
 let right = document.getElementById("rightIcon");
-let slider = document.querySelectorAll(".slider");
-let slider1 = document.getElementById("slider1");
-//let slider2 = document.getElementById("slider2");
+let imageclass = document.querySelector(".imageclass");
 
-//console.log(slider);
 
-// let imgArr = [
-//   "https://i.hizliresim.com/97s7pya.jpg",
-//   "https://i.hizliresim.com/pumz2de.jpg",
-//   "https://i.hizliresim.com/1fx5qz5.jpg",
-//   "https://i.hizliresim.com/r4jhsqm.jpg",
-//   "https://i.hizliresim.com/8gtidbw.jpg",
-// ];
+let imgArr = [
+  "https://i.hizliresim.com/97s7pya.jpg",
+  "https://i.hizliresim.com/pumz2de.jpg",
+  "https://i.hizliresim.com/du732s1.jpg",
+  "https://i.hizliresim.com/r4jhsqm.jpg",
+  "https://i.hizliresim.com/8gtidbw.jpg",
+];
 
-left.addEventListener("click", function () {
-  slider.forEach((item) => {
-    console.log(item);
-    console.log(item.previousElementSibling);
-  });
-  console.log(slider1.previousElementSibling);
-});
+let i = 0;
+imageclass.src = imgArr[0];
+function getLeftBtn() {
+  i--;
+  if (i == -1) {
+    i = imgArr.length - 1;
+  }
+  imageclass.setAttribute("src", imgArr[i]);
+}
+
+function getRightBtn() {
+  i++;
+  if (i == imgArr.length) {
+    i = 0;
+  }
+  imageclass.setAttribute("src", imgArr[i]);
+}
+
+
